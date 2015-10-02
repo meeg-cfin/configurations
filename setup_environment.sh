f44aff5f844a4763e58e9ec26f482a2d6560b53f
Christopher J. Bailey <bailey.cj@gmail.com>
2015-10-02 12:23:06 +0200

    changes to paths for ubuntu-isis

--- setup_environment.sh
+++ setup_environment.sh
@@ -9,8 +9,8 @@ current_dir="$(dirname "$BASH_SOURCE")"
 #if [[ $HOSTNAME == 'isis' ]] && [[ ${DISPLAY} ]]
 if [[ -t 0 && ( $HOSTNAME == 'isis' || $HOSTNAME == 'seth' ) ]]
 then
-	DEFAULT_PS1=${PS1}
-	DEFAULT_PATH=${PATH}
+    DEFAULT_PS1=${PS1}
+    DEFAULT_PATH=${PATH}
     DEFAULT_LD_LIBRARY_PATH=${LD_LIBRARY_PATH}
 
 	# Make a folder in /volatile for local scratch space
@@ -38,20 +38,20 @@ then
     }
 
 
-        short_path ()
-        { export PS1="\u@\h: \W> "; }
+    short_path ()
+    { export PS1="\u@\h: \W> "; }
 
-        gitprompt ()
-        {
-            source "$current_dir/bash-colors.sh"
-            source "$current_dir/git-prompt.sh"
+    gitprompt ()
+    {
+        source "$current_dir/bash-colors.sh"
+        source "$current_dir/git-prompt.sh"
 
-            export PS1="\[$Green\][\w]\[$Purple\]\$(__git_ps1)\n\[$BCyan\]\u@\[$BYellow\]\h\[\033[1;33m\] \[$White\]\$ \[$Color_Off\]";
-        }
+        export PS1="\[$Green\][\w]\[$Purple\]\$(__git_ps1)\n\[$BCyan\]\u@\[$BYellow\]\h\[\033[1;33m\] \[$White\]\$ \[$Color_Off\]";
+    }
 
-	# change this to provide a list of projects, based on group membership!
-	set_mindlabproj ()
-	{
+    # change this to provide a list of projects, based on group membership!
+    set_mindlabproj ()
+    {
         GRPLIST=(`groups`)
         PROJLIST=()
         for grp in "${GRPLIST[@]}"; do
@@ -63,9 +63,9 @@ then
                 PROJLIST+=($projname)
             fi
         done
-		if [ $# -ge 1 ]; then
+        if [ $# -ge 1 ]; then
             pnum=$1 # if you already know the number...
-		else
+        else
             echo "------------------"
             echo "Available projects"
             echo "------------------"
@@ -74,9 +74,9 @@ then
                 let ii+=1
                 echo $ii: $projname
             done
-			echo -n "Select project [1-$ii]: "
-			read pnum
-		fi
+            echo -n "Select project [1-$ii]: "
+            read pnum
+        fi
         if [ $pnum -gt ${#PROJLIST[@]} ]; then
             echo "Project number too large, what are you trying to do?"
             return 1
@@ -99,118 +99,117 @@ then
         fi
         echo "SUBJECTS_DIR set to: $SUBJECTS_DIR"
 
-	}
+    }
 
-	use ()
-	{
-		if [ $# -lt 1 ]
-		then
-			echo "Usage: use <environment name>"
-			echo "Possible environments are: anaconda, cuda, mne, mne-stable, freesurfer"
-			return 0
-		fi
-		ENV_NAME=$1
-		#ENV_VERS=$2
+    use ()
+    {
+        if [ $# -lt 1 ]
+        then
+            echo "Usage: use <environment name>"
+            echo "Possible environments are: anaconda, cuda, mne, mne-stable, freesurfer"
+            return 0
+        fi
+        ENV_NAME=$1
+        #ENV_VERS=$2
 
-      if [[ $ENV_NAME == 'anaconda' ]]
-      then
-				export PATH=/usr/local/common/anaconda/bin:$PATH
-				export MNE_PYTHON=/opt/src/python/mne-python
-				export ETS_TOOLKIT="qt4" # should make mne.gui.coregister() work
+        if [[ $ENV_NAME == 'anaconda' ]]
+        then
+            export PATH=/usr/local/common/anaconda/bin:$PATH
 
+            # up to users to set their copy of the source
+            # export MNE_PYTHON=/opt/src/python/mne-python 
 
-
-		elif [[ $ENV_NAME == 'cuda_freesurfer5.3.0' ]]
-		then
-      echo "This environment is currently not supported"
-      # use the local 5.0 installation of cuda, which FS
-      # seems to require!
-			# export PATH=/opt/local/cuda-releases/cuda-5.0/bin:$PATH
-      # export LD_LIBRARY_PATH=/opt/local/cuda-releases/cuda-5.0/lib64:${LD_LIBRARY_PATH}
+            export ETS_TOOLKIT="qt4" # should make mne.gui.coregister() work
 
 
-		elif [[ "$ENV_NAME" == mne* ]]
-		then
 
-			# MNE Configuration
-			export MNE_ROOT=/usr/local/common/${ENV_NAME}
-
-			if [ ! -d $MNE_ROOT ]
-			then
-				echo "Unknown MNE installation ($ENV_NAME), please check environment name."
-				return 1
-			fi
-
-			export MATLAB_ROOT=/usr/local/common/matlab
-			. $MNE_ROOT/bin/mne_setup_sh
-
-			# Default digital trigger line on Aarhus Triux system is:
-			export MNE_TRIGGER_CH_NAME="STI101"
-			if [ ! -d ~/.mne ]
-			then
-				echo "First time MNE-user, ~/.mne created and set up."
-				source /usr/local/common/scripts/select_Aarhus_mne_defaults.sh
-			fi
-
-			# Save the env name of mne-something while loading freesurfer
-			TMP=$ENV_NAME
-			# Get freesurfer in the path too!
-			use freesurfer
-			# Assume mne-python will be used too!
-			use anaconda
-			# But don't get double-PS1!
-			export PS1=${DEFAULT_PS1}
-			# And tell mne-something is loaded
-			export ENV_NAME=$TMP
-
-			# Set the default SUBJECTS_DIR to the MNE sample data
-			if [[ $MINDLABPROJ == 'NA' ]]
-			then
-				export SUBJECTS_DIR=/volatile/sample_data/MNE-sample-data
-			fi
-
-		elif [[ $ENV_NAME == 'freesurfer' ]]
-		then
-			#if [[ ! -z "$ENV_VERS" ]]; then
-			#	ENV_NAME=${ENV_NAME}-${ENV_VERS}
-			#fi
-			export FREESURFER_HOME=/usr/local/common/${ENV_NAME}
-
-			if [[ $MINDLABPROJ == 'NA' ]]
-			then
-				export SUBJECTS_DIR=/usr/local/sample_data/freesurfer/subjects
-			fi
-			. $FREESURFER_HOME/SetUpFreeSurfer.sh
-
-		else
-			echo "Unknown environment/programme: $ENV_NAME"
-			return 1
-		fi
+        elif [[ $ENV_NAME == cuda ]]
+        then
+            export PATH=/usr/local/cuda/bin:$PATH
 
 
-		# This gets to be too much if multiple environments are loaded...
-		#export PS1="($ENV_NAME): ${PS1}"
-		# Just leave a subtle hint we've changed something in the path...
-		#export PS1="*${PS1}"
-		if [[ ${PS1}==DEFAULT_PS1 ]]
-		then
-			export PS1="<$ENV_NAME>${PS1}"
-		else
-			echo "Multiple environments selected (use'd), only first shown..."
-		fi
-		export MINDLABENV=$ENV_NAME
-		echo "Environment '${ENV_NAME}' selected, use command 'unuse' to reset to default."
+        elif [[ "$ENV_NAME" == mne* ]]
+        then
 
-	}
+            # MNE Configuration
+            export MNE_ROOT=/usr/local/${ENV_NAME}
 
-	unuse ()
-	{
-		# Simply set path & prompt back to what it was before "use" was called
-		export PATH=${DEFAULT_PATH}
-		export PS1=${DEFAULT_PS1}
-		export LD_LIBRARY_PATH=${DEFAULT_LD_LIBRARY_PATH}
-		echo ""
-		echo "All environments unselected (last selected: '${MINDLABENV}'), path reset to default."
-		export MINDLABENV='not set'
-	}
+            if [ ! -d $MNE_ROOT ]
+            then
+                echo "Unknown MNE installation ($ENV_NAME), please check environment name."
+                return 1
+            fi
+
+            export MATLAB_ROOT=/usr/local/common/matlab
+            . $MNE_ROOT/bin/mne_setup_sh
+
+            # Default digital trigger line on Aarhus Triux system is:
+            export MNE_TRIGGER_CH_NAME="STI101"
+            if [ ! -d ~/.mne ]
+            then
+                echo "First time MNE-user, ~/.mne created and set up."
+                source /opt/local/cfin-tools/configurations/select_Aarhus_mne_defaults.sh
+            fi
+
+            # Save the env name of mne-something while loading freesurfer
+            TMP=$ENV_NAME
+            # Get freesurfer in the path too!
+            use freesurfer
+            # Assume mne-python will be used too!
+            use anaconda
+            # But don't get double-PS1!
+            export PS1=${DEFAULT_PS1}
+            # And tell mne-something is loaded
+            export ENV_NAME=$TMP
+
+            # Set the default SUBJECTS_DIR to the MNE sample data
+            if [[ $MINDLABPROJ == 'NA' ]]
+            then
+                export SUBJECTS_DIR=/volatile/sample_data/MNE-sample-data
+            fi
+
+        elif [[ $ENV_NAME == 'freesurfer' ]]
+        then
+            #if [[ ! -z "$ENV_VERS" ]]; then
+            #	ENV_NAME=${ENV_NAME}-${ENV_VERS}
+            #fi
+            export FREESURFER_HOME=/usr/local/${ENV_NAME}
+
+            if [[ $MINDLABPROJ == 'NA' ]]
+            then
+                export SUBJECTS_DIR=${FREESURFER_HOME}/subjects
+            fi
+            . $FREESURFER_HOME/SetUpFreeSurfer.sh
+
+        else
+            echo "Unknown environment/programme: $ENV_NAME"
+            return 1
+        fi
+
+
+        # This gets to be too much if multiple environments are loaded...
+        #export PS1="($ENV_NAME): ${PS1}"
+        # Just leave a subtle hint we've changed something in the path...
+        #export PS1="*${PS1}"
+        if [[ ${PS1}==DEFAULT_PS1 ]]
+        then
+            export PS1="<$ENV_NAME>${PS1}"
+        else
+            echo "Multiple environments selected (use'd), only first shown..."
+        fi
+        export MINDLABENV=$ENV_NAME
+        echo "Environment '${ENV_NAME}' selected, use command 'unuse' to reset to default."
+
+    }
+
+    unuse ()
+    {
+        # Simply set path & prompt back to what it was before "use" was called
+        export PATH=${DEFAULT_PATH}
+        export PS1=${DEFAULT_PS1}
+        export LD_LIBRARY_PATH=${DEFAULT_LD_LIBRARY_PATH}
+        echo ""
+        echo "All environments unselected (last selected: '${MINDLABENV}'), path reset to default."
+        export MINDLABENV='not set'
+    }
 fi
